1462b6650c45b6c7a398565b11305e93448d2dc7
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-12-20 21:46:59 +0200

    fetching riot account by user name and tag

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,14 @@
 import type { PageServerLoad } from './$types';
-import { API_KEY } from '$env/static/private';
+import { API_KEY, NAME, TAG } from '$env/static/private';
 
-export const load: PageServerLoad = async ({ params }) => {
-	//https://euw1.api.riotgames.com/lol/summoner/v4/summoners/me
+export const load: PageServerLoad = async ({ params, fetch }) => {
+	const response = await fetch(
+		`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${NAME}/${TAG}?api_key=${API_KEY}`
+	);
+	const data = await response.json();
+	console.log(data);
 
 	return {
-		// post: await db.getPost(params.slug)
+		post: data
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	$: puuid = data.post.puuid;
 </script>
 
-<h1>{data.post}</h1>
-<div>{@html data.post}</div>
+<h1>{puuid}</h1>
+<div>{@html puuid}</div>
