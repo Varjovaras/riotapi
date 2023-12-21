6138e1541218c57889489a43e6ecaa5d542e9643
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-12-21 22:23:06 +0200

    remove .server.ts file and move fetching into page.svelte file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { API_KEY, NAME, TAG } from '$env/static/private';
+	import { z } from 'zod';
+	import { ParticipantSchema } from '../schemas/participantSchema';
 
-	export let data: PageData;
-	// let participants = data.participants;
+	type ParticipantIds = String[];
+	type Participants = z.infer<typeof ParticipantSchema>;
 
-	$: puuid = data.post.puuid;
+	let accountApi = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${NAME}/${TAG}?api_key=${API_KEY}`;
+	let matchv5Api = `https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_6727822954?api_key=${API_KEY}`;
+
+	async function fetchMatchApi() {
+		const response = await fetch(matchv5Api);
+		const data = await response.json();
+		const participantIds: ParticipantIds = z.array(z.string()).parse(data.metadata.participants);
+
+		const participants: Participants = ParticipantSchema.parse(data.info.participants);
+
+		return {
+			participantIds,
+			participants
+		};
+	}
 </script>
 
-<h1>{puuid}</h1>
-<button
-	on:click={() => {
-		console.log('test');
-	}}>Fetch Matchv5 API</button
->
+<!-- <h1>{puuid}</h1> -->
+<button on:click={() => {}}>Fetch Matchv5 API</button>
 
-<div>{@html puuid}</div>
+<div>123</div>
