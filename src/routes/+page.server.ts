a9d3397b8013dd58d5c422695dd17519f6c7b416
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-12-21 22:15:52 +0200

    add zod validation and match v5 api route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,43 @@
 import type { PageServerLoad } from './$types';
 import { API_KEY, NAME, TAG } from '$env/static/private';
+import { z } from 'zod';
+import { ParticipantSchema } from '../schemas/participantSchema';
 
-export const load: PageServerLoad = async ({ params, fetch }) => {
-	const response = await fetch(
-		`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${NAME}/${TAG}?api_key=${API_KEY}`
-	);
+type ParticipantIds = String[];
+type Participants = z.infer<typeof ParticipantSchema>;
+
+let accountApi = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${NAME}/${TAG}?api_key=${API_KEY}`;
+let matchv5Api = `https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_6727822954?api_key=${API_KEY}`;
+
+export const load: PageServerLoad = async ({ fetch }) => {
+	const response = await fetch(accountApi);
 	const data = await response.json();
-	console.log(data);
+	const matchData = fetchMatchApi();
 
 	return {
-		post: data
+		post: data,
+		matchData: matchData
 	};
 };
+
+async function fetchMatchApi() {
+	const response = await fetch(matchv5Api);
+	const data = await response.json();
+	const participantIds: ParticipantIds = z.array(z.string()).parse(data.metadata.participants);
+
+	const participants: Participants = ParticipantSchema.parse(data.info.participants);
+
+	return {
+		participantIds,
+		participants
+	};
+}
+
+// Write data to data.json file
+// fs.writeFile('data.json', JSON.stringify(data), (err) => {
+// 	if (err) {
+// 		console.error('Error writing to data.json:', err);
+// 	} else {
+// 		console.log('Data written to data.json');
+// 	}
+// });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,16 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	// let participants = data.participants;
+
 	$: puuid = data.post.puuid;
 </script>
 
 <h1>{puuid}</h1>
+<button
+	on:click={() => {
+		console.log('test');
+	}}>Fetch Matchv5 API</button
+>
+
 <div>{@html puuid}</div>
